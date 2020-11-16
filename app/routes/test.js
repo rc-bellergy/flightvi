8a03c33dac99c5d996a4a151760ef57b9549a590
Bellergy RC <rc@bellergy.com>
2020-11-16 20:02:47 +0800

    init express js

--- app/routes/test.js
+++ app/routes/test.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET test page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Flightvie' });
+  res.render('test', { title: 'Test' });
 });
 
 module.exports = router;
